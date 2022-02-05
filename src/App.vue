0d85fcd4b562fa2b336bac15ba97e9c5b29aa806
RoRux <rorux84@gmail.com>
2022-02-05 15:55:09 +0300

    init project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>Пятнашки</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <game-field />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import GameField from "./components/GameField";
+
+export default {
+  name: "App",
+
+  components: {
+    GameField,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
